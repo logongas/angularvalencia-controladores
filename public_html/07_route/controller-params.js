d9866f7abd98630bf4ebf165033aefc0e0a293b7
logongas <lorenzo.profesor@gmail.com>
2015-11-18 23:38:41 +0100

    Poner el controlador dentro de la ruta como una función en vez de que sea un controlador aparte

--- public_html/07_route/controller-params.js
+++ public_html/07_route/controller-params.js
@@ -2,15 +2,11 @@ app.decorator("$route",function($delegate) {
     var $route=$delegate;
     for(var route in $route.routes) {
         if (!$route.routes[route].controller) {
-            $route.routes[route].controller="ControllerParams";
+            $route.routes[route].controller=['$scope','controllerParams',function($scope,controllerParams) {
+                    angular.extend($scope,controllerParams);
+            }];
         }
     }
     
     return $delegate;
 });
-
-ControllerParams.$inject=['$scope','controllerParams'];
-function ControllerParams($scope,controllerParams) {
-    angular.extend($scope,controllerParams)
-}
-app.controller("ControllerParams",ControllerParams);
