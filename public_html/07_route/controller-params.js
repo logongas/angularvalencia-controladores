68f4c0e400d0827c566a095ce6eac17dcb73c82e
logongas <lorenzo.profesor@gmail.com>
2015-11-18 23:42:04 +0100

    Solo creamos el controlador de la ruta si ademas está el resolve.controllerParams

--- public_html/07_route/controller-params.js
+++ public_html/07_route/controller-params.js
@@ -1,7 +1,7 @@
 app.decorator("$route",function($delegate) {
     var $route=$delegate;
     for(var route in $route.routes) {
-        if (!$route.routes[route].controller) {
+        if ((!$route.routes[route].controller) && ($route.routes[route].resolve) && ($route.routes[route].resolve.controllerParams)) {
             $route.routes[route].controller=['$scope','controllerParams',function($scope,controllerParams) {
                     angular.extend($scope,controllerParams);
             }];
